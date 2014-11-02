f2c1180a79986dcdcfa36049f5a2fc4d2c285d99
Wei Zhong <zhongwei99@163.com>
2014-11-02 14:38:05 +0800

    Add all source code of chapter 1.

--- include/apue.h
+++ include/apue.h
@@ -1,4 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <unistd.h>
+#include <string.h>
+
 #define MAXLINE	4096			/* max line length */
+
+void	err_quit(const char *, ...) __attribute__((noreturn));
+
+void	err_ret(const char *, ...);
+void	err_sys(const char *, ...) __attribute__((noreturn));
--- lib/error.c
+++ lib/error.c
@@ -4,6 +4,14 @@
 
 static void err_doit(int, int, const char *, va_list);
 
+void err_ret(const char *fmt, ...) {
+	va_list		ap;
+
+	va_start(ap, fmt);
+	err_doit(1, errno, fmt, ap);
+	va_end(ap);
+}
+
 /*
  * Fatal error related to a system call.
  * Print a message and terminate.
